035a354177d79e487a35f3db5deb3ce5b2e6d7ac
m-siegel <87347495+m-siegel@users.noreply.github.com>
2022-04-18 20:43:30 -0700

    preliminary methods

--- PlayerInterface.java
+++ PlayerInterface.java
@@ -1,1 +1,6 @@
-interface PlayerInterface {}
+interface PlayerInterface {
+  
+  // return coordinates of the tip of the launcher (where the projectile should appear)
+  public int getLaunchX();
+  public int getLaunchY();
+}
