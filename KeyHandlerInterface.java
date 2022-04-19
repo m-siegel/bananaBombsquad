de5f7d76338da0e8bdca76f2898435a26877e238
m-siegel <mea.siegel@gmail.com>
2022-04-18 20:03:05 -0700

    preliminary methods

--- KeyHandlerInterface.java
+++ KeyHandlerInterface.java
@@ -1,1 +1,14 @@
-interface KeyHandlerInterface {}
+interface KeyHandlerInterface {
+
+  /** Returns value of each instance variable indicating whether
+    * the keys for each action are pressed.
+    */
+  public boolean angleUp(); // or angleLeft() or angleCounterClockwise()
+
+  public boolean angleDown();
+
+  public boolean powerUp();
+
+  public boolean powerDown();
+  
+}
