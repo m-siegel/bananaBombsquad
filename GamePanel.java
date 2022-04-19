cddcab414054a09b5ccda599237d56a6000ab1f2
arm2349 <56358399+arm2349@users.noreply.github.com>
2022-04-18 18:27:25 -0700

    Update GamePanel.java

--- GamePanel.java
+++ GamePanel.java
@@ -1,14 +1,27 @@
 import javax.swing.JPanel;
 
 public class GamePanel extends JPanel implements Runnable{
-
+    
+    //settings
     public static final int TILE_SIZE = 48;
     public static final int SCREEN_WIDTH = 1536;
     public static final int SCREEN_HEIGHT = 1152;
     public static final int FPS = 60;
     
+    
+    //instance variables
     private Thread gameThread;
-
+    private KeyHandler keyH;
+    
+    //constructors, getters, setters
+    public GamePanel() {
+        this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
+        this.addKeyListener(keyH);
+        
+    }
+    
+    
+    //methods
     public void loadSprites();
 
     
