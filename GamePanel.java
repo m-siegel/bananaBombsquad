9ad2aac2c44f88b0caee48da25530aedbe5439aa
arm2349 <56358399+arm2349@users.noreply.github.com>
2022-04-18 17:52:06 -0700

    Add files via upload
    the first few preliminary ideas for a GamePanel skeleton

---
+++ GamePanel.java
@@ -0,0 +1,29 @@
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel implements Runnable{
+
+    public static final int TILE_SIZE = 48;
+    public static final int SCREEN_WIDTH = 1536;
+    public static final int SCREEN_HEIGHT = 1152;
+    public static final int FPS = 60;
+
+    public void loadSprites();
+
+    public void adjustAngleAndPower();
+
+
+    public void update() {
+        initiateLaunch();
+    }
+
+    public void initiateLaunch();
+
+
+
+    @Override
+    public void run() {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
