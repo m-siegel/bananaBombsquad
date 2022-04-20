d4ecfd1949162d69750468e65960140ddb530c5f
arm2349 <56358399+arm2349@users.noreply.github.com>
2022-04-19 20:49:13 -0700

    Update Target.java

--- gameSprites/Target.java
+++ gameSprites/Target.java
@@ -3,20 +3,24 @@ import java.util.ArrayList;
 import java.util.Random;
 import main.GamePanel;
 import spriteEssentials.*;
+//import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
 
 public class Target extends Sprite {
      // instance variables
      private int numberOfHits;
+     //private boolean isHit;
 
      public Target() {
          this.x = getRandomXCoordinate();
          this.y = getRandomYCoordinate();
-         this.speed = 0;
+         this.speed = 5;
          this.keyHandler = null;
          this.solid = true;
          this.numberOfHits = 0;
-         this.images = new ArrayList<>();
+         this.images = new ArrayList<BufferedImage>();
+         //this.isHit = false;
      }
 
      
@@ -46,7 +50,12 @@ public class Target extends Sprite {
         return randomInt;
      }
 
-     public void update() {}
+     public void update() {
+         //set new random position for target
+         this.x = getRandomXCoordinate();
+         this.y = getRandomYCoordinate();
+
+     }
 
      public void draw(Graphics2D g2) {
         switch (this.numberOfHits) {
