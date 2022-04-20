80b988e5c0636bc5a53a3a629f24b621f033f169
m-siegel <mea.siegel@gmail.com>
2022-04-19 18:05:22 -0700

    bare bones Sprite

---
+++ utilities/HitBox.java
@@ -0,0 +1,13 @@
+package utilities;
+
+/**
+ * Represents a Sprite's hitbox with the min and max x and y dimensions of it's solid area.
+ */
+public class HitBox {
+  private int xMin;
+  private int xMax;
+  private int yMin;
+  private int yMax;
+
+  public HitBox
+}
