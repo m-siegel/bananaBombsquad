71c64599a98496311082b52c7259918064529505
Tim Crawley <tim.crawley12@gmail.com>
2022-04-19 20:15:11 -0700

    updated SpriteList

--- spriteEssentials/SpriteList.java
+++ spriteEssentials/SpriteList.java
@@ -1,7 +1,19 @@
 package spriteEssentials;
 
 import java.util.ArrayList;
+import java.awt.Graphics2D;
 
 public class SpriteList extends ArrayList<Sprite> {
-  
+
+    public void update() {
+        for (Sprite x : this) {
+            x.update();
+        }
+    }
+
+    public void draw(Graphics2D g2) {
+        for (Sprite x : this) {
+            x.draw(g2);
+        }
+    }
 }
