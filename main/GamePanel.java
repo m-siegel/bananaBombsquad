eec8a3f1d3c898fa1d4c62aab4e37d894e278ddd
Tim Crawley <tim.crawley12@gmail.com>
2022-04-19 18:48:48 -0700

    Updated GamePanel class. Methods still in progress

--- main/GamePanel.java
+++ main/GamePanel.java
@@ -1,38 +1,52 @@
 package main;
 import javax.swing.JPanel;
-import sprites.Sprite;
+import sprites.*;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
 
 public class GamePanel extends JPanel implements Runnable{
     
     //settings
     public static final int TILE_SIZE = 48;
-    public static final int SCREEN_WIDTH = 1536;
-    public static final int SCREEN_HEIGHT = 1152;
+    public static final int SCALE = 4;
+    public static final int SCREEN_WIDTH = TILE_SIZE * SCALE * 4;
+    public static final int SCREEN_HEIGHT = TILE_SIZE * SCALE * 3;
     public static final int FPS = 60;
     
     
     //instance variables
     private Thread gameThread;
     private KeyHandler keyH;
-    private Sprite player;
-    private Sprite background;
-    private Sprite target;
-    private Sprite projectile;
+    private Cannon cannon;
+    private Background background;
+    private Target target;
+    private Projectile projectile;
+    private Wall wall;
+    private Ground ground;
     
     //constructors, getters, setters
     public GamePanel() {
         this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
         this.addKeyListener(keyH);
+        this.setDoubleBuffered(true);
+        this.setFocusable(true);
+
         
     }
     
     
     //methods
-    public void loadSprites();
+    public void loadSprites() {
+
+    }
 
     
     /* requires KeyHandler */
-    public void adjustAngleAndPower();
+    public void adjustAngleAndPower() {
+
+    }
 
 
     public void update() {
@@ -51,13 +65,35 @@ public class GamePanel extends JPanel implements Runnable{
 
     @Override
     public void run() {
-        // TODO Auto-generated method stub
         
         //Game loop goes here
-        
-        update();
-        repaint();
-        
+        double drawInterval = 1000000000 / FPS;
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
+
+        while (gameThread != null) {
+
+            currentTime = System.nanoTime();
+            delta += (currentTime - lastTime) / drawInterval;
+            lastTime = currentTime;
+
+            if (delta >= 1) {
+                update();
+                repaint();
+                delta--;
+            }
+        }
     }
     
+    public void paintComponent(Graphics g) {
+
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D)g;
+
+        // draw the different elements
+        //cannon.draw(g2);
+        
+
+    }
 }
