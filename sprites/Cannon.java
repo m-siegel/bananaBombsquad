63d796fcdee65384cca03b275c63730d57690d99
m-siegel <87347495+m-siegel@users.noreply.github.com>
2022-04-20 19:52:16 -0700

    Delete sprites directory
    Switching over to gameSprites directory

--- sprites/Cannon.java
+++
@@ -1,8 +0,0 @@
-package sprites;
-
-public class Cannon extends Sprite {
-  
-  // return coordinates of the tip of the launcher (where the projectile should appear)
-  public int getLaunchX();
-  public int getLaunchY();
-}
--- sprites/Wall.java
+++
@@ -1,5 +0,0 @@
-package sprites;
-
-public class Wall extends Sprite {
-    
-}
