d96027f3b091a336d09b23233a586579b2172f27
m-siegel <mea.siegel@gmail.com>
2022-04-20 21:50:10 -0700

    first draft Projectile

--- gameSprites/Projectile.java
+++ gameSprites/Projectile.java
@@ -7,26 +7,36 @@ import java.awt.image.BufferedImage;
 
 public class Projectile extends Sprite {
 
-    public Projectile(double angle, int x, int y, int velocity, ArrayList<BufferedImage> images) {
+    public Projectile(int x, int y, double angle, int velocity, ArrayList<BufferedImage> images, int updatesPerSec) {
         if (images == null) {
             throw new NullPointerException("Cannot instantiate null images.");
         }
-
+        if (images.size() < 1) {
+            throw new IllegalArgumentException(
+                    "Cannot instantiate with an empty images ArrayList");
+        }
         this.images = Sprite.copyBufferedImages(images);
         this.splattered = false;
-        this.angle = angle;
         this.x = x;
         this.y = y;
+        this.angle = angle;
+        this.velocity = velocity;
+
+        // TODO -- comment why this is
         this.internalX = 0;
         this.internalY = 0; 
         this.xDisplacement = this.x - this.internalX;
         this.yDisplacement = this.y - this.internalY;
-        this.velocity = velocity;
+
         this.solid = true;
         this.keyHandler = null;
         this.speed = 5;
+        this.imagesIndex = 0;
+        this.updatesSinceFrameChange = 0;
+        this.updatesPerFrame = updatesPerSec / FPS;
     }
     //instance variables
+    public static final int FPS = 30;
 
     private boolean splattered;
     private double angle;
@@ -36,10 +46,14 @@ public class Projectile extends Sprite {
     private int xDisplacement;
     private int yDisplacement;
 
+    private int updatesSinceFrameChange;
+    private int updatesPerFrame;
+
     //getters
     public boolean isSplattered() {
         return this.splattered;
     }
+
     public double getAngle() {return this.angle;}
     public int getVelocity() {return this.velocity;}
     public int getInternalX() {return this.internalX;}
@@ -47,23 +61,53 @@ public class Projectile extends Sprite {
     public int getXDisplacement() {return this.xDisplacement;}
     public int getYDisplacement() {return this.yDisplacement;}
 
-
-
-
     public void update() {
-        this.internalX += this.speed;
-        this.internalY = (int) Math.round( Math.tan(angle * (Math.PI / 180) ) - (9.8 * (internalX * internalX / 
-            (2 * velocity * velocity * Math.cos(angle * (Math.PI / 180) ) * Math.cos(angle * (Math.PI / 180) ))) ));
+        updatePosition();
+        rotateImage();
+    }
 
-        //formula: y = h + tan(A) - ( 9.8 * ( x^2 / (2v^2 * cos(A)* cos(A)) )
+    private void updatePosition() {
+        // Find projectile position relative to start at (0, 0) internally (draw arc)
+        this.internalX += this.speed; // update x over time
+        // find y in terms of x
+        // don't bother with height because coerced starting point to be (0, 0), height == 0
+        // 9.8 for gravity -- TODO -- static var
+        this.internalY = (int) ((internalX * Math.tan(Math.toRadians(angle))) - (9.8 * Math.pow(internalX, 2) /
+                (2 * Math.pow(velocity, 2) * Math.pow(Math.cos(Math.toRadians(angle)), 2))));
+
+        // this.internalY = (int) Math.round( Math.tan(angle * (Math.PI / 180) ) - (9.8 * (internalX * internalX / 
+        //     (2 * velocity * velocity * Math.cos(angle * (Math.PI / 180) ) * Math.cos(angle * (Math.PI / 180) ))) ));
+
+        //formula in deg: y = h + (x)tan(A) - ( 9.8 * ( x^2 / (2v^2 * cos(A)* cos(A)) )
         //h = 0
-        
+
+        // Find actual position
+        this.x = internalX + xDisplacement;
+        this.y = internalY + yDisplacement;
+    }
+
+    private void rotateImage() {
+        updatesSinceFrameChange++;
+        if (updatesSinceFrameChange >= updatesPerFrame) {
+            // update frame
+            this.imagesIndex = (imagesIndex + 1) % images.size(); // cycle through images
+            updatesSinceFrameChange = 0;
+        }
     }
 
     public void draw(Graphics2D g2) {
-        //x-coordinate: x = internalX + xDisplacement
-        //y-coordinate: 
-        g2.drawImage(images.get(0), this.x, this.y, null); //
+        g2.drawImage(images.get(imagesIndex), this.x, this.y, null);
     }
 
+    @Override
+    public HitBox getHitBox() {
+        if (!isSolid()) {
+            return null;
+        }
+        int width = images.get(imagesIndex).getWidth();
+        int height = images.get(imagesIndex).getHeight();
+        // TODO -- make scale factor
+        return new HitBox(x - (int) (width * .1), x + (int) (width * 1.1),
+                y - (int) (height * .1), y + (int) (height * 1.1));
+    }
 }
