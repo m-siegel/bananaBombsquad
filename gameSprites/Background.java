54f82ce505a526fe6744b6c1a61cb5c3f79a1083
cerolopez <98296063+cerolopez@users.noreply.github.com>
2022-04-21 18:45:04 -0700

    Update background

--- gameSprites/Background.java
+++ gameSprites/Background.java
@@ -1,6 +1,16 @@
 package gameSprites;
 import spriteEssentials.*;
+import java.awt.image.BufferedImage;
+import java.awt.Graphics2D;
 
 public class Background extends Sprite {
-    
+
+    public Background(int x, int y, BufferedImage image) {
+        super(x, y, image);
+    }
+
+    @Override
+    public void draw(Graphics2D g2) {
+        g2.drawImage(super.images.get(0), null, super.getX(), super.getY());
+    }    
 }
