d2431f6febd548b645a7be66f2dc7618db848ebd
arm2349 <56358399+arm2349@users.noreply.github.com>
2022-04-21 21:29:59 -0700

    Create .gitignore

--- main/InstructionPanel.java
+++ main/InstructionPanel.java
@@ -2,8 +2,19 @@ package main;
 
 import javax.swing.JPanel;
 import javax.swing.JButton;
+import java.awt.Color;
+import java.awt.Dimension;
 
 public class InstructionPanel extends JPanel{
+    public InstructionPanel(String buttonText) {
+        super();
+        JButton button = new JButton(buttonText);
+        button.setBackground(Color.ORANGE);
+        button.addMouseListener(new MouseHandler());
+        this.add(button);
+        this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
+        this.setBackground(Color.PINK);
+    }
 
     //settings - same as GamePanel
     public static final int TILE_SIZE = 48;
@@ -12,7 +23,7 @@ public class InstructionPanel extends JPanel{
     public static final int SCREEN_HEIGHT = TILE_SIZE * SCALE * 3;
     public static final int FPS = 60;
 
-    JButton button = new JButton("Let's Play!");
+
     
 
     
