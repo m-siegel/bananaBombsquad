b699bbcce7b5f2ac4ad80e0c0fae6f6634f0d1a8
Tim Crawley <tim.crawley12@gmail.com>
2022-04-21 19:17:30 -0700

    added constructor and draw method

--- gameSprites/Wall.java
+++ gameSprites/Wall.java
@@ -1,6 +1,22 @@
 package gameSprites;
+import main.GamePanel;
 import spriteEssentials.*;
+import java.awt.image.BufferedImage;
+import java.awt.Graphics2D;
 
 public class Wall extends Sprite {
     
+    public Wall(int x, int y, BufferedImage image) {
+        super(x, y, image);
+    }
+
+    @Override
+    public void draw(Graphics2D g2) {
+        int tempX = GamePanel.SCREEN_WIDTH - images.get(0).getWidth();
+        int tempY = 0;
+        while (tempY < GamePanel.SCREEN_HEIGHT) {
+            g2.drawImage(images.get(0), null, tempX, tempY);
+            tempY += images.get(0).getHeight(); 
+        }
+    }
 }
