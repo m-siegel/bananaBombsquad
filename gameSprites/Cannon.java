b56b1e508503a6e86f8f3fe9a95906e1aafb9b28
m-siegel <mea.siegel@gmail.com>
2022-04-23 16:19:13 -0700

    change null to illegalarg in constructor

--- gameSprites/Cannon.java
+++ gameSprites/Cannon.java
@@ -57,13 +57,13 @@ public class Cannon extends Sprite {
    */
   public Cannon(int x, int y, BufferedImage wheel, BufferedImage barrel, KeyHandler keyHandler) {
     if (keyHandler == null) {
-      throw new NullPointerException("keyHandler parameter cannot be null.");
+      throw new IllegalArgumentException("keyHandler parameter cannot be null.");
     }
     if (wheel == null) {
-      throw new NullPointerException("wheel parameter cannot be null.");
+      throw new IllegalArgumentException("wheel parameter cannot be null.");
     }
     if (barrel == null) {
-      throw new NullPointerException("barrel parameter cannot be null.");
+      throw new IllegalArgumentException("barrel parameter cannot be null.");
     }
     if (wheel.getWidth() != wheel.getHeight()) {
       throw new IllegalArgumentException("wheel image must have equal height and width");
