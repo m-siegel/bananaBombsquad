f3c0a724adb0bbd16563f36d42d00bf81615128c
arm2349 <56358399+arm2349@users.noreply.github.com>
2022-04-24 21:09:22 -0700

    fixed imagesIndex issue in Target

--- gameSprites/Target.java
+++ gameSprites/Target.java
@@ -12,8 +12,6 @@ public class Target extends Sprite {
 
     public static final int MAX_HITS = 3;
 
-    private int numberOfHits;
-
     public Target(ArrayList<BufferedImage> images) {
         if (images == null) {
             throw new NullPointerException("Cannot instantiate a target with null images.");
@@ -26,41 +24,45 @@ public class Target extends Sprite {
         this.speed = 0;
         this.keyHandler = null;
         this.solid = true;
-        this.numberOfHits = 0;
-        this.imagesIndex = 0; // Will not be used
+        this.imagesIndex = 0;
         this.images = Sprite.copyBufferedImages(images);
     }
 
+    public int getNumberOfHits() {
+        return this.imagesIndex;
+    }
+
     public boolean incrementNumberOfHits() {
-        if (this.numberOfHits >= 3) { // if already full, do not increment
+        if (this.imagesIndex >= 3) { // if already full, do not increment
             return false;
         }
-        this.numberOfHits++;
+        this.imagesIndex++;
         return true;
     }
 
     public boolean isFull() {
-        return this.numberOfHits >= 3;
+        return this.imagesIndex >= 3;
     }
 
     public int getRandomXCoordinate() {
         Random randomizer = new Random();
         int oneThirdsScreen = GamePanel.SCREEN_WIDTH / 3;
-        int randomInt = randomizer.nextInt(oneThirdsScreen - this.images.get(numberOfHits).getWidth());
+        int randomInt = randomizer.nextInt(oneThirdsScreen 
+                - this.images.get(getNumberOfHits()).getWidth());
         return randomInt + 2 * oneThirdsScreen;
     }
 
     public int getRandomYCoordinate() {
         Random randomizer = new Random();
+        // random int between 0 - screen height, inclusive
         int randomInt = randomizer.nextInt(
-                GamePanel.SCREEN_HEIGHT - this.images.get(numberOfHits).getHeight()); // random int between 0 - screen
-                                                                                      // height, inclusive
+                GamePanel.SCREEN_HEIGHT - this.images.get(getNumberOfHits()).getHeight()); 
         return randomInt;
     }
 
     @Override
     public void draw(Graphics2D g2) {
-        g2.drawImage(images.get(numberOfHits), this.x, this.y, null);
+        g2.drawImage(images.get(getNumberOfHits()), this.x, this.y, null);
     }
 
     public void resetPosition() {
@@ -69,7 +71,7 @@ public class Target extends Sprite {
     }
 
     public void resetHits() {
-        this.numberOfHits = 0;
+        this.imagesIndex = 0;
     }
 
     public void reset() {
