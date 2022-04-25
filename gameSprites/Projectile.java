13c4ada434374d90db2e2050e71655a166d8f55e
Tim Crawley <tim.crawley12@gmail.com>
2022-04-24 20:46:36 -0700

    cleaned up projectile file

--- gameSprites/Projectile.java
+++ gameSprites/Projectile.java
@@ -1,15 +1,32 @@
 package gameSprites;
 
 import java.util.ArrayList;
-
-import spriteEssentials.*;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 
+import spriteEssentials.*;
+
 public class Projectile extends Sprite {
 
-    public Projectile(int x, int y, double angle, int velocity, ArrayList<BufferedImage> flyingImages,
-            ArrayList<BufferedImage> splatteredImages, int updatesPerSec) {
+    // instance variables
+    public static final int FPS = 30;
+    public static final double GRAVITY = 9.8;
+
+    private boolean splattered;
+    private double angle;
+    private int velocity;
+    private int internalX;
+    private int internalY;
+    private int xDisplacement;
+    private int yDisplacement;
+    private ArrayList<BufferedImage> splatteredImages;
+    private ArrayList<BufferedImage> flyingImages;
+    private int updatesSinceFrameChange;
+    private int updatesPerFrame;
+
+    public Projectile(int x, int y, double angle, int velocity,
+            ArrayList<BufferedImage> flyingImages, ArrayList<BufferedImage> splatteredImages,
+            int updatesPerSec) {
         if (flyingImages == null) {
             throw new NullPointerException("Cannot instantiate with null flyingImages.");
         }
@@ -36,9 +53,9 @@ public class Projectile extends Sprite {
         this.angle = angle;
         this.velocity = velocity * 5; // divide by 100 to slow it down a bit
 
-        // TODO -- comment why this is
+        // used to calculate flight path of projectile
         this.internalX = 0;
-        this.internalY = 0; 
+        this.internalY = 0;
         this.xDisplacement = this.x - this.internalX;
         this.yDisplacement = this.y - this.internalY;
 
@@ -49,63 +66,62 @@ public class Projectile extends Sprite {
         this.updatesSinceFrameChange = 0;
         this.updatesPerFrame = FPS / updatesPerSec;
     }
-    //instance variables
-    public static final int FPS = 30;
 
-    private boolean splattered;
-    private double angle;
-    private int velocity;
-    private int internalX;
-    private int internalY;
-    private int xDisplacement;
-    private int yDisplacement;
-    private ArrayList<BufferedImage> splatteredImages;
-    private ArrayList<BufferedImage> flyingImages;
-
-    private int updatesSinceFrameChange;
-    private int updatesPerFrame;
-
-    //getters
     public boolean isSplattered() {
         return this.splattered;
     }
-    public void splat() { // TODO -- v2: rotate images if splattered against a wall (so they make more sense)
+
+    public void splat() { // case when image is splattered on the ground
+        this.splat(0);
+    }
+
+    public void splat(int rotation) { // TODO -- v2: rotate images if splattered against a wall (so
+                                      // they make more sense)
         this.splattered = true;
         this.imagesIndex = 0;
         this.images = splatteredImages;
     }
 
-    public double getAngle() {return this.angle;}
-    public int getVelocity() {return this.velocity;}
-    public int getInternalX() {return this.internalX;}
-    public int getInternalY() {return this.internalY;}
-    public int getXDisplacement() {return this.xDisplacement;}
-    public int getYDisplacement() {return this.yDisplacement;}
+    public double getAngle() {
+        return this.angle;
+    }
+
+    public int getVelocity() {
+        return this.velocity;
+    }
+
+    public int getInternalX() {
+        return this.internalX;
+    }
+
+    public int getInternalY() {
+        return this.internalY;
+    }
+
+    public int getXDisplacement() {
+        return this.xDisplacement;
+    }
+
+    public int getYDisplacement() {
+        return this.yDisplacement;
+    }
 
     public void update() {
-        if (! this.isSplattered()) {
+        if (!this.isSplattered()) {
             updatePosition();
         }
         animateImage();
     }
 
     private void updatePosition() {
-        // Find projectile position relative to start at (0, 0) internally (draw arc)
+        // Find projectile position relative to start at cartesian (0, 0) internally (draw arc)
         this.internalX += this.speed; // update x over time
-        // find y in terms of x
-        // don't bother with height because coerced starting point to be (0, 0), height == 0
-        // 9.8 for gravity -- TODO -- static var
-
-        //this.internalY -= this.speed;
-
-        this.internalY = (int) ((internalX * Math.tan(Math.toRadians(angle))) - (9.8 * Math.pow(internalX, 2) /
-                (2 * Math.pow(velocity, 2) * Math.pow(Math.cos(Math.toRadians(angle)), 2))));
-
-        // this.internalY = (int) Math.round( Math.tan(angle * (Math.PI / 180) ) - (9.8 * (internalX * internalX / 
-        //     (2 * velocity * velocity * Math.cos(angle * (Math.PI / 180) ) * Math.cos(angle * (Math.PI / 180) ))) ));
-
-        //formula in deg: y = h + (x)tan(A) - ( 9.8 * ( x^2 / (2v^2 * cos(A)* cos(A)) )
-        //h = 0
+        
+        // formula in deg: y = h + (x)tan(A) - ( 9.8 * ( x^2 / (2v^2 * cos(A)* cos(A)) )
+        // h = 0
+        this.internalY = (int) ((internalX * Math.tan(Math.toRadians(angle))) 
+                - (GRAVITY * Math.pow(internalX, 2)
+                / (2 * Math.pow(velocity, 2) * Math.pow(Math.cos(Math.toRadians(angle)), 2))));
 
         // Find actual position
         this.x = internalX + xDisplacement;
@@ -132,8 +148,7 @@ public class Projectile extends Sprite {
         }
         int width = images.get(imagesIndex).getWidth();
         int height = images.get(imagesIndex).getHeight();
-        // TODO -- make scale factor
-        return new HitBox(x - (int) (width * .1), x + (int) (width * 1.1),
-                y - (int) (height * .1), y + (int) (height * 1.1));
+        return new HitBox(x - (int) (width * .1), x + (int) (width * 1.1), y - (int) (height * .1),
+                y + (int) (height * 1.1));
     }
 }
